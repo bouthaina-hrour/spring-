818f1c6c57971c63df42c36655b635b556940813
bouthaina-hrour <73646814+bouthaina-hrour@users.noreply.github.com>
2021-11-16 18:42:38 +0100

    populating some data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO BUILDING(id,outside_temperature) VALUES(-10,10.0);
 INSERT INTO ROOM(id, name, floor, current_temperature, target_temperature,building_id) VALUES(-10, 'Hall', 1, 22.3, 20.0,-10);
-INSERT INTO ROOM(id, name, floor,building_id) VALUES(-9, 'Amphi A', -10,-10);
+INSERT INTO ROOM(id, name, floor,building_id) VALUES(-9, 'AmphiA', -10,-10);
 
 INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-10, 'ON', 'Heater1', 2000, -10);
 INSERT INTO HEATER(id, heater_status, name, power, room_id) VALUES(-9, 'ON', 'Heater2', null, -10);
